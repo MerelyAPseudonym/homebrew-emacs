47209394776cdba8c0366fe97ce0574e65cf7ebe
Josh Tilles <josh@signafire.com>
2016-09-21 09:45:20 -0400

    savegame

--- Formula/projectile.rb
+++ Formula/projectile.rb
@@ -7,10 +7,32 @@ class Projectile < EmacsFormula
   sha256 "c7417e25f2fc113194ca68aaecb1a6fe55e44734d5ab0fd643ba7eb0511779d6"
   head "https://github.com/bbatsov/projectile.git"
 
+<<<<<<< Updated upstream
   depends_on :emacs => "24.1"
   depends_on "homebrew/emacs/dash-emacs"
   depends_on "homebrew/emacs/epl"
+=======
+  option "with-helm", "Helm integration for Projectile"
+  option "with-persp", "Perspective integration for Projectile"
+  
+  depends_on "homebrew/emacs/dash"
+>>>>>>> Stashed changes
   depends_on "homebrew/emacs/pkg-info"
+
+  depends_on "homebrew/emacs/helm" if build.with? "helm"
+  # depends_on "homebrew/emacs/perspective" if build.with? "persp"
+
+  resource "helm-projectile" do
+    url "https://raw.githubusercontent.com/bbatsov/projectile/v0.12.0/helm-projectile.el"
+    sha256 "0ba641c02d5897d2702711f411a3a2f6399a8b8576b8dc700f64125425aa2019"
+  end
+
+  resource "persp-projectile" do
+    url "https://raw.githubusercontent.com/bbatsov/projectile/v0.12.0/persp-projectile.el"
+    sha256 "4e4552178674765983e8a43aa7cc0bfa5a15ef16e7169b13504a186740d8a6fa"
+  end
+
+
 
   def install
     byte_compile "projectile.el"
